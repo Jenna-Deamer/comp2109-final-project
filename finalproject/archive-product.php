5355360052ac1676ec83179a8d84d1b2da52a186
Jenna Deamer <jennadeamer@gmail.com>
2024-04-04 19:32:24 -0400

    custom shop page styles

--- finalproject/archive-product.php
+++ finalproject/archive-product.php
@@ -3,16 +3,17 @@ get_header();
 $shopFeaturedImg = wp_get_attachment_image_src( get_post_thumbnail_id( wc_get_page_id( 'shop' ) ), 'full' );
 ?>
     <section class="shop-masthead" style="background: url('<?php echo $shopFeaturedImg[0]; ?>');">
-        <div>
-            <h1 class="text-center">Shop Page</h1>
+        <div class="shop-masthead-content">
+            <h1 class="text-center">Explore Our Selection</h1>
         </div>
     </section>
-    <section class="shopContainer">
-        <?php
-        do_action('woocommerce_before_shop_loop');
-        echo do_shortcode ('[products limit="6" columns="3"]');
-        do_action('woocommerce_after_shop_loop');
-        ?>
+    <section class="container pt-4">
+
+            <?php
+            do_action('woocommerce_before_shop_loop');
+            echo do_shortcode('[products columns="3"]');
+            do_action('woocommerce_after_shop_loop');
+            ?>
     </section>
 <?php
 get_footer();
