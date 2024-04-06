cd032030336a5439c7ea56c556ca85b10aab3da7
Jenna Deamer <jennadeamer@gmail.com>
2024-04-05 21:13:30 -0400

    added google fonts

--- finalproject/templates/custom-cart-template.php
+++ finalproject/templates/custom-cart-template.php
@@ -9,7 +9,7 @@ $cartFeaturedImg = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID(
 <main class="cart-container">
     <section class="masthead" style="background-image: url('<?php echo esc_url($cartFeaturedImg[0]); ?>')">
         <div class="masthead-content container">
-            <h1><?php echo wp_kses_post(get_field('page_title')); ?></h1>
+            <h1>Your Cart</h1>
         </div>
     </section>
     <section class="cart-body container">
