a06b9ca726a6d9e47536a5181151f8f0becba1e7
Jenna Deamer <jennadeamer@gmail.com>
2024-04-05 20:50:55 -0400

    checkout styles

---
+++ finalproject/templates/custom-checkout.template.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Custom Checkout Template
+ */
+get_header();
+$checkoutFeaturedImg = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full');
+?>
+
+<main class="checkout-container">
+    <section class="masthead" style="background-image: url('<?php echo esc_url($checkoutFeaturedImg[0]); ?>')">
+        <div class="masthead-content container">
+            <h1>Checkout</h1>
+        </div>
+    </section> <!-- Close masthead section here -->
+    <section class="checkout-body container">
+        <?php
+        // Output WooCommerce checkout form
+        echo do_shortcode('[woocommerce_checkout]');
+        ?>
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
