733f3919234c344c7e63df322253eb94b901f0ad
Jenna Deamer <jennadeamer@gmail.com>
2024-04-11 21:46:51 -0400

    material page styles

--- finalproject/templates/custom-learning-materials-template.php
+++ finalproject/templates/custom-learning-materials-template.php
@@ -13,34 +13,40 @@ $learningMaterialFeaturedImg = wp_get_attachment_image_src(get_post_thumbnail_id
         </div>
     </section>
 
-    <section class="learning-material-page row">
-        <?php
-        //get all learning material posts by matching slug
-        $learning_materials_query = new WP_Query(array(
-            'post_type' => 'learning_material',
-            'posts_per_page' => -1,
-        ));
-
-        //if there are posts. loop through & display
-        if ($learning_materials_query->have_posts()) :
-            while ($learning_materials_query->have_posts()) : $learning_materials_query->the_post();
-                // Get the post thumbnail (featured image)
-                $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail');
-                ?>
-                <article class="learning-material-post col-md-4 col-sm-6">
-                    <?php if ($thumbnail_url) : ?>
-                        <img src="<?php echo esc_url($thumbnail_url); ?>" alt="<?php the_title_attribute(); ?>" class="learning-material-thumbnail">
-                    <?php endif; ?>
-                    <h2><?php the_title(); ?></h2>
-                    <div class="learning-material-excerpt">
-                        <?php the_excerpt(); ?>
-                    </div>
-                </article>
+    <section class="learning-material-page">
+        <div class="material-post-container">
             <?php
-            endwhile;
-            wp_reset_postdata();
-        endif;
-        ?>
+            //get all learning material posts by matching slug
+            $learning_materials_query = new WP_Query(array(
+                'post_type' => 'learning_material',
+                'posts_per_page' => -1,
+            ));
+
+            //if there are posts. loop through & display
+            if ($learning_materials_query->have_posts()) :
+                while ($learning_materials_query->have_posts()) : $learning_materials_query->the_post();
+                    // Get the post thumbnail (featured image)
+                    $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail');
+                    ?>
+                    <article class="learning-material-post">
+                        <h3><?php the_title(); ?></h3>
+
+                        <?php if ($thumbnail_url) : ?>
+                            <img src="<?php echo esc_url($thumbnail_url); ?>" alt="<?php the_title_attribute(); ?>">
+                        <?php endif; ?>
+
+                        <div class="learning-material-excerpt">
+                            <?php the_excerpt(); ?>
+                        </div>
+                        <a href="<?php the_permalink(); ?>" class="material-button">View Full Article</a>
+                    </article>
+                <?php
+                endwhile;
+                wp_reset_postdata();
+            endif;
+            ?>
+        </div>
+
 
     </section>
 </main>
